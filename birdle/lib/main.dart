fead987371294938d596a857729ada312ab35f5e
Joginder Mikael <jogindermikael@gmail.com>
2026-08-07 20:05:45 +0300

    rows and colums

--- birdle/lib/main.dart
+++ birdle/lib/main.dart
@@ -10,42 +10,74 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: const Align(
+            alignment: Alignment.centerLeft,
+            child: Text("Birdle"),
+          ),
+        ),
         body: Center(
-          child: Tile('A', HitType.partial),
+          child: GamePage(),
         ),
       ),
     );
   }
 }
 
-class Tile extends StatelessWidget{
+class Tile extends StatelessWidget {
   const Tile(this.letter, this.hitType, {super.key});
 
   final String letter;
   final HitType hitType;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
       width: 60,
       height: 60,
       decoration: BoxDecoration(
         border: Border.all(color: Colors.grey.shade300),
-        color: switch(hitType){
+        color: switch (hitType) {
           HitType.hit => Colors.green,
           HitType.partial => Colors.yellow,
           HitType.miss => Colors.grey,
           _ => Colors.white,
-        }
+        },
       ),
       child: Center(
         child: Text(
           letter.toUpperCase(),
           style: Theme.of(context).textTheme.titleLarge,
-        )
-      )
+        ),
+      ),
+    );
+  }
+}
+
+
+class GamePage extends StatelessWidget {
+  GamePage({super.key});
+
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 5.0,
+        children: [
+          for (final guess in _game.guesses)
+            Row(
+              spacing: 5.0,
+              children: [
+
+              ]
+            ),
+        ],
+      ),
     );
   }
 }
