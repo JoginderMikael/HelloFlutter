4d22edeec59975bec7cb3ce4aadfebea6b71d792
Joginder Mikael <jogindermikael@gmail.com>
2026-08-07 09:34:56 +0300

    loops in dart

---
+++ starter/loops.dart
@@ -0,0 +1,24 @@
+void main(){
+// For loops
+var num = 5;
+
+for(var i=0; i<num; i++){
+  print(i);
+}
+
+
+//For in Loops
+
+var names = ["Joginder", "John", "Mary"];
+for(var name in names){
+  print(name);
+}
+
+//while Loops
+var j = 0;
+while(j < num){
+  print(j);
+  j++;
+}
+
+}
