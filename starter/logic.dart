c059f8f55559896fc72bdd6192b8fc5c38b38f1b
Joginder Mikael <jogindermikael@gmail.com>
2026-08-07 09:41:41 +0300

    logics in dart

---
+++ starter/logic.dart
@@ -0,0 +1,23 @@
+void main(){
+  //If statements
+  var num = 5;
+  if(num == 5){
+    print("The number is 5");
+  }
+
+  //if/else
+  if(num > 5){
+    print("The number is greater than 5");
+  } else {
+    print("The number is less than or equal to 5");
+  }
+
+  //else if
+  if(num > 5){
+    print("The number is greater than 5");
+  } else if(num < 5){
+    print("The number is less than 5");
+  } else {
+    print("The number is equal to 5");
+  }
+}
