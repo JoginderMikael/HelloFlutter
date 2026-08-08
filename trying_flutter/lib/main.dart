4eae710bc7809b358c97ee729176a8a4093b2510
Joginder Mikael <jogindermikael@gmail.com>
2026-08-08 11:01:28 +0300

    padding widget

--- trying_flutter/lib/main.dart
+++ trying_flutter/lib/main.dart
@@ -15,11 +15,10 @@ class Home extends StatelessWidget{
       centerTitle: true,
       backgroundColor: Colors.blue,
     ),
-    body: Container(
-      padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-      margin: EdgeInsets.all(20.0),
-      color: Colors.grey[400],
-      child: Text("Hello"),
+    body: Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Text('Hello World'),
+   
     ),
      
     floatingActionButton: FloatingActionButton(
