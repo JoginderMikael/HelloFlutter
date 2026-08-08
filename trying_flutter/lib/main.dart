fa38f8ba69d01b5ebd951bc4c68344d2c4be0218
Joginder Mikael <jogindermikael@gmail.com>
2026-08-08 10:29:53 +0300

    adding image

--- trying_flutter/lib/main.dart
+++ trying_flutter/lib/main.dart
@@ -16,17 +16,22 @@ class Home extends StatelessWidget{
       backgroundColor: Colors.blue,
     ),
     body: Center(
-      child: Text(
-        'Hello Joginder!',
-        style: TextStyle(
-          fontSize: 24.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-          fontFamily: 'IndieFlower',
-          ),
+      child: Image(
+        image: AssetImage('assets/night3.jpg'),
       ),
-    ),
+     
+     // child: Text(
+      //   'Hello Joginder!',
+      //   style: TextStyle(
+      //     fontSize: 24.0,
+      //     fontWeight: FontWeight.bold,
+      //     letterSpacing: 2.0,
+      //     color: Colors.grey[600],
+      //     fontFamily: 'IndieFlower',
+      //     ),
+
+      ),
+    // ),
     floatingActionButton: FloatingActionButton(
       onPressed: () {
         // Add your onPressed code here!
