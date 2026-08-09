a3eea409d39e7ee0385a0e4981c1cba17746ff1e
Joginder Mikael <jogindermikael@gmail.com>
2026-08-09 22:03:24 +0300

    creating card

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -1,41 +1,57 @@
 import 'package:flutter/material.dart';
 import 'quotes.dart';
 
-void main() => runApp(MaterialApp(
-  home: QuotesList(),
-));
+void main() => runApp(MaterialApp(home: QuotesList()));
 
 class QuotesList extends StatefulWidget {
   const QuotesList({super.key});
-
 
   @override
   State<QuotesList> createState() => _QuotesListState();
 }
 
 class _QuotesListState extends State<QuotesList> {
-  
   List<Quote> quotes = [
     Quote(author: 'Arthur', text: 'I am Old testament'),
     Quote(author: 'Lee', text: 'Knowledge is Power!'),
     Quote(author: 'Tommy', text: 'Big fucks Small'),
-];
+  ];
+
+  Widget quotesTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(fontSize: 18.0, color: Colors.grey[600]),
+            ),
+            SizedBox(height: 6.0),
+            Text(
+              quote.author,
+              style: TextStyle(fontSize: 14.0, color: Colors.grey[800]),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text(
-          "Awesome Quotes",
-          style: TextStyle(color: Colors.white),
-          ),
+        title: Text("Awesome Quotes", style: TextStyle(color: Colors.white)),
         centerTitle: true,
         backgroundColor: Colors.redAccent,
       ),
-      body:Column(
-        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
-      )
+      body: Column(
+        children: quotes.map((quote) => quotesTemplate(quote)).toList(),
+      ),
     );
   }
 }
