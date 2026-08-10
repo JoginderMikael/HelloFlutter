ff6e61a21f5a2d395a4ba3c282e91cdb7147409f
Joginder Mikael <jogindermikael@gmail.com>
2026-08-10 16:02:54 +0300

    Error handling

--- world_time/lib/services/world_time.dart
+++ world_time/lib/services/world_time.dart
@@ -7,32 +7,35 @@ class WorldTime {
   String flag; //URL to an asset flag icon
   String url; //location url for api endpoint
 
-WorldTime({ required this.location, required this.flag,  required this.url });
-    
-    Future<void> getTime() async{
-    
-    //make the request
-    Response response = await get(Uri.parse('https://timeapi.io/api/v1/time/current/zone?timezone=$url'));
+  WorldTime({required this.location, required this.flag, required this.url});
 
-    Map data = jsonDecode(response.body);
-    // print(data);
+  Future<void> getTime() async {
+    try {
+      //make the request
+      Response response = await get(
+        Uri.parse('https://timeapi.io/api/v1/time/current/zone?timezone=$url'),
+      );
 
-    //Get properties from data
+      Map data = jsonDecode(response.body);
+      // print(data);
 
-    String datetime = data['date_time'];
-    int offset = data['utc_offset_seconds'];
+      //Get properties from data
 
-    // print(datetime);
-    //  print(offset);
+      String datetime = data['date_time'];
+      int offset = data['utc_offset_seconds'];
 
-    //create a date time object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(seconds: offset));
+      // print(datetime);
+      //  print(offset);
 
-    //set the time property
-    time = now.toString();
+      //create a date time object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(seconds: offset));
+
+      //set the time property
+      time = now.toString();
+    } catch (e) {
+      print('Caught error $e');
+      time = 'Could not get time data';
+    }
   }
-
 }
-
-
