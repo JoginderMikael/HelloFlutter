04020626f7d3942fff06d1bf6799232c6369af82
Joginder Mikael <jogindermikael@gmail.com>
2026-08-10 16:40:33 +0300

    display the time on the screen

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
 
@@ -10,13 +12,15 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  String? time = 'loading....';
+
   
   void setupWorldTime() async{
     WorldTime instance = WorldTime(location: 'Nairobi', flag: 'Kenya.png', url: 'Africa/Nairobi');
     await instance.getTime();
-    setState(() {
-      time = instance.time;
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
     });
     
   }
@@ -33,7 +37,7 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(50.0),
-        child: Text(time!),
+        child: Text("Loading...."),
         ),
     );
   }
