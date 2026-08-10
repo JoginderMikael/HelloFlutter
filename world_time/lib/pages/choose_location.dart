c4bf72e1b5e2d0743abac90740764b93727d64bd
Joginder Mikael <jogindermikael@gmail.com>
2026-08-10 12:35:17 +0300

    initstate and set states

--- world_time/lib/pages/choose_location.dart
+++ world_time/lib/pages/choose_location.dart
@@ -8,8 +8,18 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  int counter = 0;
+
+@override
+  void initState(){
+    super.initState();
+    print('initState function ran');
+}
+
   @override
   Widget build(BuildContext context) {
+    print('build function ran');
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -21,7 +31,14 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Text("Choose Location Screen"),
+      body: ElevatedButton(
+        onPressed: (){
+          setState(() {
+            counter++;
+          });
+        }, 
+        child: Text('counter is $counter'),
+        )
     );
   }
 }
