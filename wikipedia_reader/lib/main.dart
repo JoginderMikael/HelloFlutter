99695e4d817e872d1449f7a9fc43b4c5e10c6886
Joginder Mikael <jogindermikael@gmail.com>
2026-08-19 23:10:35 +0300

    notifier

--- wikipedia_reader/lib/main.dart
+++ wikipedia_reader/lib/main.dart
@@ -15,11 +15,64 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final viewModel = ArticleViewModel(ArticleModel());
+
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Wikipedia Flutter')),
-        body: const Center(child: Text('Loading...')),
+        body: const Center(child: Text('Check Console for article data')),
       ),
     );
   }
+}
+
+class ArticleModel{
+  Future<Summary> getRandomArticleSummary() async{
+    final uri = Uri.https(
+      'en.wikipedia.org',
+      '/api/rest_v1/page/random/summary',
+    );
+
+    final response = await get(uri);
+
+    if(response.statusCode != 200){
+      throw const HttpException('Failed to update resource');
+    }
+
+    return Summary.fromJson(jsonDecode(response.body) as Map<String, Object?>);
+  }
+}
+
+
+class ArticleViewModel extends ChangeNotifier{
+  final ArticleModel model;
+  Summary? summary;
+  Exception? error;
+  bool isLoading = false;
+
+
+  ArticleViewModel(this.model){
+    fetchArticle();
+  }
+
+Future<void> fetchArticle() async{
+  isLoading = true;
+  notifyListeners();
+
+  try{
+    summary = await model.getRandomArticleSummary();
+    print('Article loaded: ${summary!.titles.normalized}');
+    error = null;
+  } on HttpException catch(e){
+    print('Error loading article: ${e.message}');
+    error = e;
+    summary = null;
+  }
+
+  isLoading = false;
+  notifyListeners();
+}
+
 }
